0d25b2c4520e154881107548f5302319fed140ed
ADP-24 <aperez@appdev.yearup.org>
2024-10-18 15:32:39 -0500

    Any updated elements

--- market_research.sql
+++ market_research.sql
@@ -1,5 +1,7 @@
 SELECT * FROM google_trends;
 
+-- Displays trends of google search for 'Whey Protein Isolate' from the years of 2004 to 2024
+
 SELECT year, Whey_Protein_Isolate,
     LAG(Whey_Protein_Isolate) OVER (ORDER BY year) AS Previous_Month,
     (Whey_Protein_Isolate - LAG(Whey_Protein_Isolate) OVER (ORDER BY year)) AS Increase_Amount,
@@ -10,6 +12,8 @@ SELECT year, Whey_Protein_Isolate,
     END AS Change_Type
 FROM google_trends
 ORDER BY year;
+
+-- Displays trends of google search for 'Protein' from the years of 2004 to 2024
 
 SELECT year, Protein_Macro,
     LAG(Protein_Macro) OVER (ORDER BY year) AS Previous_Month,
@@ -24,20 +28,28 @@ ORDER BY year;
 
 SELECT * FROM fitness_memberships_census;
 
+-- Displays the last 5 years of the highest expenditure in fitness memberships
+
 SELECT Year, Value 
 FROM fitness_memberships_census
 ORDER BY Value DESC
 LIMIT 5;
 
-SELECT AVG(Value) AS avg_value_2010_to_2017
-FROM fitness_memberships_census
-WHERE Year BETWEEN 2010 AND 2017;
+-- A incorperated subquery that average value of expenditure between years.
 
-SELECT AVG(Value) AS avg_value_2018_to_2022
-FROM fitness_memberships_census
-WHERE Year BETWEEN 2018 AND 2022;
+SELECT 
+    (SELECT AVG(Value) 
+     FROM fitness_memberships_census 
+     WHERE Year BETWEEN 2010 AND 2017) AS avg_value_2010_to_2017,
+     
+    (SELECT AVG(Value) 
+     FROM fitness_memberships_census 
+     WHERE Year BETWEEN 2018 AND 2022) AS avg_value_2018_to_2022;
+
 
 SELECT * FROM whey_exports;
+
+-- Volume in (Metric Tons) for each year for China and the Percentage of change from he previous year.
 
 SELECT 
     Country,
@@ -51,12 +63,14 @@ SELECT
 FROM whey_exports
 WHERE Country = 'China';
 
+-- Displays the (Metric Tons) for each year for All countries in 'Total' and the Percentage of change from the previous year.
+
 SELECT 
     'All Countries' AS Country,
-    SUM(2019_Metric_Tons) AS total_2019_Metric_Tons,
-    SUM(2020_Metric_Tons) AS total_2020_Metric_Tons,
-    SUM(2021_Metric_Tons) AS total_2021_Metric_Tons,
-    SUM(2022_Metric_Tons) AS total_2022_Metric_Tons,
+    SUM(2019_Metric_Tons) AS Total_2019_Metric_Tons,
+    SUM(2020_Metric_Tons) AS Total_2020_Metric_Tons,
+    SUM(2021_Metric_Tons) AS Total_2021_Metric_Tons,
+    SUM(2022_Metric_Tons) AS Total_2022_Metric_Tons,
     ROUND(((SUM(2020_Metric_Tons) - SUM(2019_Metric_Tons)) / SUM(2019_Metric_Tons)) * 100, 1) AS growth_2019_to_2020,
     ROUND(((SUM(2021_Metric_Tons) - SUM(2020_Metric_Tons)) / SUM(2020_Metric_Tons)) * 100, 1) AS growth_2020_to_2021,
     ROUND(((SUM(2022_Metric_Tons) - SUM(2021_Metric_Tons)) / SUM(2021_Metric_Tons)) * 100, 1) AS growth_2021_to_2022
